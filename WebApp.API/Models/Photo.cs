910855d47be7fed2d49eac2c49679b4686c18bc0
“sanjoy” <sanjoy@dohatec.com.bd>
2023-04-09 14:50:16 +0600

    Extend User Class with some new fields

---
+++ WebApp.API/Models/Photo.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Discription { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+        public User User { get; set; }
+        public int UserID { get; set; }
+    }
+}
